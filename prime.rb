5041bb427e1b1ee9c146fe655dd370292bc0ed71
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 00:59:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num < 2 
+    return false
+  end 
+  top = num / 2 
+  (2..top).each do |n| 
+    if num % n == 0 
+      return FALSE
+    end
+  end
+  return true 
+end
